10cef99f20232f4b36959db59cb1424f205ac234
Alexander Egard <veryhasted@gmail.com>
2024-11-09 23:33:18 +0100

    Update ItemRosterRegistry.cs

--- source/GameInterface/Services/ItemRosters/ItemRosterRegistry.cs
+++ source/GameInterface/Services/ItemRosters/ItemRosterRegistry.cs
@@ -1,6 +1,9 @@
 ﻿using GameInterface.Services.Registry;
 using System.Threading;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Roster;
+using TaleWorlds.CampaignSystem.Settlements;
 
 namespace GameInterface.Services.ItemRosters
 {
@@ -12,7 +15,27 @@ namespace GameInterface.Services.ItemRosters
 
         public override void RegisterAll()
         {
-            //TODO
+            var objectManager = Campaign.Current?.CampaignObjectManager;
+
+            if (objectManager == null)
+            {
+                Logger.Error("Unable to register objects when CampaignObjectManager is null");
+                return;
+            }
+
+            foreach (MobileParty party in objectManager.MobileParties)
+            {
+                if (party.ItemRoster == null) continue;
+
+                RegisterNewObject(party.ItemRoster, out var _);
+            }
+
+            foreach(Settlement settlement in objectManager.Settlements)
+            {
+                if (settlement.ItemRoster == null) continue;
+
+                RegisterNewObject(settlement.ItemRoster, out var _);
+            }
         }
 
         protected override string GetNewId(ItemRoster itemRoster)
