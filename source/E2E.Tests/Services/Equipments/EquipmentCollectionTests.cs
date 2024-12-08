3d4136ecd63c48e4dc82f43a0f03a5e1a796d687
Georgy <Georgy.rudnev@deefinity.com>
2024-12-08 14:44:11 +0100

    Collection sync, lifetime refactor

--- source/E2E.Tests/Services/Equipments/EquipmentCollectionTests.cs
+++ source/E2E.Tests/Services/Equipments/EquipmentCollectionTests.cs
@@ -2,6 +2,12 @@
 using Xunit.Abstractions;
 using TaleWorlds.Core;
 using HarmonyLib;
+using E2E.Tests.Environment.Instance;
+using GameInterface.Services.Equipments.Patches;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Siege;
+using System.Xml.Linq;
+using TaleWorlds.ObjectSystem;
 
 
 
@@ -11,6 +17,9 @@ namespace E2E.Tests.Services.Equipments;
 public class EquipmentCollectionTests : IDisposable
 {
     E2ETestEnvironment TestEnvironment { get; }
+    private string ItemObjectId;
+    private string EquipmentId;
+
     public EquipmentCollectionTests(ITestOutputHelper output)
     {
         TestEnvironment = new E2ETestEnvironment(output);
@@ -23,71 +32,75 @@ public class EquipmentCollectionTests : IDisposable
 
     // TODO create tests similar to Sync BesiegerCamp #977 or Item roster synchronization #701
 
-
     [Fact]
     public void ServerUpdateEquipmentCollection_SyncAllClients()
     {
         // Arrange
         var server = TestEnvironment.Server;
-
-        string? EquipmentId = null;
-
-
+        EquipmentElement element = new EquipmentElement();
         // Act
 
         server.Call(() =>
         {
-            // No Params
-            var Equip = new Equipment();
-            Assert.True(server.ObjectManager.TryGetId(Equip, out EquipmentId));
-
-
+            EquipmentId = TestEnvironment.CreateRegisteredObject<Equipment>();
+            ItemObjectId = TestEnvironment.CreateRegisteredObject<ItemObject>();
+            Assert.True(server.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var Equipment));
+            Assert.True(server.ObjectManager.TryGetObject<ItemObject>(ItemObjectId, out var serverItemObject));
+            EquipmentElement element = new EquipmentElement(serverItemObject);
+            EquipmentCollectionPatches.ArrayAssignIntercept(Equipment._itemSlots, 0, element, Equipment);
+            Assert.Equal(element.Item, Equipment._itemSlots[0].Item);
         });
 
         // Assert
         Assert.True(server.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var Equip));
 
-
-
         foreach (var client in TestEnvironment.Clients)
         {
-            Assert.True(client.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var _));
 
+            Assert.True(client.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var clientEquipment));
+            Assert.Equal(element.Item, clientEquipment._itemSlots[0].Item);
 
         }
 
     }
-
 
     [Fact]
     public void ClientUpdateEquipmentCollection_DoesNothing()
     {
         // Arrange
         var server = TestEnvironment.Server;
-        var client1 = TestEnvironment.Clients.First();
 
+        EquipmentElement element = new EquipmentElement();
 
         server.Call(() =>
         {
-           
+            EquipmentId = TestEnvironment.CreateRegisteredObject<Equipment>();
+            ItemObjectId = TestEnvironment.CreateRegisteredObject<ItemObject>();
+            Assert.True(server.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var serverEquipment));
+            Assert.True(server.ObjectManager.TryGetObject<ItemObject>(ItemObjectId, out var serverItemObject));
+            element = new EquipmentElement(serverItemObject);
         });
-
-        // Act
-
-
-        client1.Call(() =>
-        {
-
-
-        });
-
-        // Assert
-
 
         foreach (var client in TestEnvironment.Clients)
         {
+            Assert.True(client.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var syncedEquipment));
+        }
 
+        // Act
+        var firstClient = TestEnvironment.Clients.First();
+        firstClient.Call(() =>
+        {
+            Assert.True(server.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var clientEquipment));
+            Assert.True(server.ObjectManager.TryGetObject<ItemObject>(ItemObjectId, out var clientItemObject));
+            EquipmentCollectionPatches.ArrayAssignIntercept(clientEquipment._itemSlots, 0, element, clientEquipment);
+            
+        });
 
+        // Assert
+        foreach (var client in TestEnvironment.Clients)
+        {
+            Assert.True(client.ObjectManager.TryGetObject<Equipment>(EquipmentId, out var clientEquipment));
+            Assert.NotEqual(element.Item, clientEquipment._itemSlots[0].Item);
         }
     }
 
